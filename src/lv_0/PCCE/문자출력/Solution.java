c18b9054c429e3ed58bc0bd4e85c42b9f31e35dd
A540 <wlsdud2528@gmail.com>
2025-06-20 18:06:22 +0900

    PCCE 1~5

---
+++ src/lv_0/PCCE/문자출력/Solution.java
@@ -0,0 +1,10 @@
+package lv_0.PCCE.문자출력;
+
+public class Solution {
+    public static void main(String[] args) {
+        String Message = "Let's go";
+
+        System.out.println("3\n2\n1");
+        System.out.println(Message);
+    }
+}
