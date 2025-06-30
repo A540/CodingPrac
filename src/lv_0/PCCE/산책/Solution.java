10781779d287536104c8ba833d5d448a8745774b
[CLOUD4] 박진영 <wlsdud2528@naver.com>
2025-06-30 20:05:26 +0900

    PCCE 10공원, 산책, 창고정리, 출력

---
+++ src/lv_0/PCCE/산책/Solution.java
@@ -0,0 +1,32 @@
+package lv_0.PCCE.산책;
+
+public class Solution {
+    public int[] solution(String route){
+        int east = 0;
+        int north = 0;
+        int[] answer = new int[2];
+        for(int i = 0; i < route.length(); i++){
+            switch(route.charAt(i)){
+                case 'N':
+                    north++;
+                    break;
+
+                case 'E':
+                    east++;
+                    break;
+
+                case 'S':
+                    north--;
+                    break;
+
+                case 'W':
+                    east--;
+                    break;
+            }
+            answer[0] = east;
+            answer[1] = north;
+        }
+
+        return answer;
+    }
+}
